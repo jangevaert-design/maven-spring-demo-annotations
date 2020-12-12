2e648a8480b626b60357cca1893505df9eff383c
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-12 06:17:01 -0700

    Implemented the @Qualifier annotation.

--- src/main/java/edu/cnm/deepdive/springdemo/MotorcycleCoach.java
+++ src/main/java/edu/cnm/deepdive/springdemo/MotorcycleCoach.java
@@ -1,25 +1,30 @@
 package edu.cnm.deepdive.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MotorcycleCoach implements Coach {
 
+  @Autowired
+  @Qualifier("databaseFortuneService")
   private FortuneService fortuneService;
 
   @Autowired
-  public MotorcycleCoach(FortuneService theFortuneService) {
+  public MotorcycleCoach(@Qualifier("databaseFortuneService") FortuneService theFortuneService) {
     this.fortuneService = theFortuneService;
   }
 
   @Override
   public String getDailyWorkout() {
-    return "Do some wheelies";
+    return "\nDo some wheelies";
   }
 
   @Override
   public String getDailyFortune() {
     return fortuneService.getFortune();
   }
+
+
 }
