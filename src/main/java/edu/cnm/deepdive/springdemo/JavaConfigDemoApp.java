d1aae1fb1a9a44e072017f6c61646fdf6c5e4e3d
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-13 09:42:17 -0700

    workked on Spring configuration with Java Code (no xml).

---
+++ src/main/java/edu/cnm/deepdive/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+  public static void main(String[] args) {
+    // read spring config java class
+    AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(SportConfig.class);
+    //get the bean from the spring container
+    Coach theCoach = context.getBean("tennisCoach", Coach.class);
+    //call a method on the bean
+    System.out.println(theCoach.getDailyWorkout());
+    //call method to get the daily fortune
+    System.out.println("\n" + theCoach.getDailyFortune());
+    //close the context
+    context.close();
+  }
+}
---
+++ src/main/java/edu/cnm/deepdive/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.cnm.deepdive.springdemo")
+public class SportConfig {
+
+}
